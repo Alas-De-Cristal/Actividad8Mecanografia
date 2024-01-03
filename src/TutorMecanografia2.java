0721571336f50b92eb54aa5047d746b2d64dcd73
MarthaTayan <mtayan1353@utm.edu.ec>
2024-01-02 19:03:02 -0500

    Pangrama Seleccionado

--- src/TutorMecanografia2.java
+++ src/TutorMecanografia2.java
@@ -22,6 +22,7 @@ public class TutorMecanografia2 extends JFrame {
         // Crear el teclado virtual
         JPanel keyboardPanel = new JPanel(new GridLayout(4, 10));
         String[] buttonLabels = {
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
                 "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
                 "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ",
                 "Z", "X", "C", "V", "B", "N", "M"
